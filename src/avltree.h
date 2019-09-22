97a78c92259c609908ebb3f0321d4f091b0d66fd
Mahmoud Mohsen <mahmoud.mohsen.213@outlook.com>
2019-09-22 20:30:40 +0200

    bug fixes

--- src/avltree.h
+++ src/avltree.h
@@ -2,9 +2,9 @@
 using namespace std;
 
 template<class type>
-class tree{
+class tree {
 private:
-  class node{
+  class node {
   public:
     node *left;
     node *right;
@@ -33,114 +33,114 @@ public:
 
 ///////////////////////////////////////
 template<class type>
-tree<type>::node::node(){
+tree<type>::node::node() {
   left = NULL;
   right = NULL;
   height = 0;
 }
 
 template<class type>
-tree<type>::node::node(node *_left, node *_right, type _value){
+tree<type>::node::node(node *_left, node *_right, type _value) {
   left = _left;
   right = _right;
   value = _value;
-  height = max(getHeight(left),getHeight(right))+1;
+  height = max(getHeight(left), getHeight(right)) + 1;
 }
 
 template<class type>
-int tree<type>::node::getHeight(node *n){
-  if(n == NULL)
+int tree<type>::node::getHeight(node *n) {
+  if (n == NULL)
     return 0;
   return n->height;
 }
 
 template<class type>
-int tree<type>::node::getBalance(){
+int tree<type>::node::getBalance() {
+  if (!left && !right) return 0;
+  if (!left) return -right->height;
+  if (!right) return left->height;
   return left->height - right->height;
 }
 
 ///////////////////////////////////////
 
 template<class type>
-tree<type>::tree(){
+tree<type>::tree() {
   root = NULL;
 }
 
 template<class type>
-tree<type>::~tree(){
+tree<type>::~tree() {
   this->clear(root);
   root = NULL;
 }
 
 template<class type>
-typename tree<type>::node* tree<type>::rotateLeft(node *n){
+typename tree<type>::node* tree<type>::rotateLeft(node *n) {
   node *rightChild = n->right;
   node *leftGrandChild = rightChild->left;
 
   rightChild->left = n;
   n->right = leftGrandChild;
 
-  n->height = max(n->getHeight(left),
-            n->getHeight(right))+1;
-  rightChild->height = max(rightChild->getHeight(left),
-                rightChild->getHeight(right))+1;
+  n->height = max(n->getHeight(n->left), n->getHeight(n->right)) + 1;
+  rightChild->height = max(rightChild->getHeight(rightChild->left),
+      rightChild->getHeight(rightChild->right)) + 1;
 
   return rightChild;
 }
 
 template<class type>
-typename tree<type>::node* tree<type>::rotateRight(node *n){
+typename tree<type>::node* tree<type>::rotateRight(node *n) {
   node *leftChild = n->left;
   node *rightGrandChild = leftChild->right;
 
   leftChild->right = n;
   n->left = rightGrandChild;
 
-  n->height = max(n->getHeight(left),
-              n->getHeight(right))+1;
-  leftChild->height = max(leftChild->getHeight(left),
-                leftChild->getHeight(right))+1;
+  n->height = max(n->getHeight(n->left), n->getHeight(n->right)) + 1;
+  leftChild->height = max(leftChild->getHeight(leftChild->left),
+      leftChild->getHeight(leftChild->right)) + 1;
 
   return leftChild;
 }
 
 template<class type>
-void tree<type>::insert(type value){
-  if(root == NULL)
-    root = new node(NULL,NULL,value);
-  else root = insert(value,root);
+void tree<type>::insert(type value) {
+  if (root == NULL)
+    root = new node(NULL, NULL, value);
+  else root = insert(value, root);
 }
 
 template<class type>
-typename tree<type>::node* tree<type>::insert(type value,
-                      node *current){
-  if(current == NULL)
-    return new node(NULL,NULL,value);
-  if(value == current->value)
+typename tree<type>::node* tree<type>::insert(type value, node *current) {
+  if (current == NULL)
+    return new node(NULL, NULL, value);
+  if (value == current->value)
     return current;
 
-  if(value < current->value)
+  if (value < current->value)
     current->left = insert(value, current->left);
   else current->right = insert(value, current->right);
 
-  current->height = max(current->getHeight(left),
-              current->getHeight(right))+1;
+  current->height = max(current->getHeight(current->left),
+      current->getHeight(current->right)) + 1;
 
   int balance = current->getBalance();
 
   // Left Left Case
-  if(balance > 1 && value < current->left->value)
+  if (balance > 1 && value < current->left->value)
     return rotateRight(current);
   // Right Right Case
   else if (balance < -1 && value > current->right->value)
     return rotateLeft(current);
   // Left Right Case
-  else if (balance > 1 && value > current->left->value){
-    current->left =  rotateLeft(current->left);
+  else if (balance > 1 && value > current->left->value) {
+    current->left = rotateLeft(current->left);
     return rotateRight(current);
   }
   // Right Left Case
-  else if (balance < -1 && value < current->right->value){
+  else if (balance < -1 && value < current->right->value) {
     current->right = rotateRight(current->right);
     return rotateLeft(current);
   }
@@ -149,62 +149,62 @@ typename tree<type>::node* tree<type>::insert(type value,
 }
 
 template<class type>
-void tree<type>::erase(type value){
-  root = erase(value,root);
+void tree<type>::erase(type value) {
+  root = erase(value, root);
 }
 
 template<class type>
-typename tree<type>::node* tree<type>::erase(type value,
-                    node *current){
-  if(current == NULL)
+typename tree<type>::node* tree<type>::erase(type value, node *current) {
+  if (current == NULL)
     return NULL;
 
-  if(value < current->value)
-    current->left = erase(value,current->left);
-  else if(value > current->value)
-    current->right = erase(value,current->right);
-  else{
-    if(current->left == NULL){
+  if (value < current->value)
+    current->left = erase(value, current->left);
+  else if (value > current->value)
+    current->right = erase(value, current->right);
+  else {
+    if (current->left == NULL) {
       node* temp = current;
       current = current->right;
       delete temp;
-    }
-    else if(current->right == NULL){
+      return current;
+    } else if (current->right == NULL) {
       node* temp = current;
       current = current->left;
       delete temp;
-    }
-    else{
+      return current;
+    } else {
       node *leftMost = current->right;
-      while(leftMost->left != NULL){
+      while (leftMost->left != NULL)
         leftMost = leftMost->left;
-      }
 
       current->value = leftMost->value;
-      current->right = erase(leftMost->value,current->right);
+      current->right = erase(leftMost->value, current->right);
     }
   }
 
-  if(current == NULL)
+  if (current == NULL)
     return NULL;
 
-  current->height = max(current->getHeight(left),
-              current->getHeight(right))+1;
+  current->height = max(current->getHeight(current->left),
+      current->getHeight(current->right)) + 1;
   int balance = current->getBalance();
+  int leftBalance = (balance > 0) ? current->left->getBalance() : 0;
+  int rightBalance = (balance < 0) ? current->right->getBalance() : 0;
 
   // Left Left Case
-  if(balance > 1 && value < current->left->value)
+  if (balance > 1 && leftBalance >= 0)
     return rotateRight(current);
   // Right Right Case
-  else if (balance < -1 && value > current->right->value)
+  else if (balance < -1 && rightBalance <= 0)
     return rotateLeft(current);
   // Left Right Case
-  else if (balance > 1 && value > current->left->value){
-    current->left =  rotateLeft(current->left);
+  else if (balance > 1) {
+    current->left = rotateLeft(current->left);
     return rotateRight(current);
   }
   // Right Left Case
-  else if (balance < -1 && value < current->right->value){
+  else if (balance < -1) {
     current->right = rotateRight(current->right);
     return rotateLeft(current);
   }
@@ -213,13 +213,13 @@ typename tree<type>::node* tree<type>::erase(type value,
 }
 
 template<class type>
-bool tree<type>::find(type value){
+bool tree<type>::find(type value) {
   node *current = root;
-  while(current!=NULL){
-    if(value == current->value)
+  while (current != NULL) {
+    if (value == current->value)
       return true;
 
-    if(value < current->value)
+    if (value < current->value)
       current = current->left;
     else current = current->right;
   }
@@ -228,9 +228,8 @@ bool tree<type>::find(type value){
 }
 
 template<class type>
-void tree<type>::clear(node *current){
-  if(current == NULL)
-    return;
+void tree<type>::clear(node *current) {
+  if (current == NULL) return;
   clear(current->left);
   clear(current->right);
   delete current;
